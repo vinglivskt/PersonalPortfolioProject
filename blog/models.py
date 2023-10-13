82da9fcfc7db5b90dfa57bcc11db8271147869c7
Igor Vinglivskt <vinglivskt@gmail.com>
2023-10-13 12:34:14 +0300

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(max_length=250)
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
